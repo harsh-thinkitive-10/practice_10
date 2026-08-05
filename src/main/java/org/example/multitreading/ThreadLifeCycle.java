bed16a49777a602cdba76b78aa97622e5b382a68
Harsh Mahajan <harsh.mahajan@thinkitive.com>
2026-08-05 14:17:01 +0530

    added thread life cycle on 05-08-26

---
+++ src/main/java/org/example/multitreading/ThreadLifeCycle.java
@@ -0,0 +1,45 @@
+package org.example.multitreading;
+
+public class ThreadLifeCycle {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new New_Thread());
+        System.out.println(thread.getState());
+
+        thread.start();
+        System.out.println(thread.getState());
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            synchronized (thread){
+                thread.wait(100);
+                System.out.println(thread.getState());
+
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(thread.isAlive());
+
+        System.out.println(thread.getState());
+
+    }
+}
+
+class New_Thread implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Thread is running.");
+            Thread.sleep(1000);
+            System.out.println("Thread is terminate.");
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
